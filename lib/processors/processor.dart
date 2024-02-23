d60ef72a28022912986ff46bca8038672f013fbc
Jhoanse <jsilva9796@gmail.com>
2024-02-22 19:40:47 -0500

    processor class is updated

--- lib/processors/processor.dart
+++ lib/processors/processor.dart
@@ -1,29 +1,66 @@
 class Processor {
-  List<String> pertain(List<String> a, List<String> b) {
-    return [];
+  bool pertain(List<String> a, List<String> b) {
+    Set<String> setA = a.toSet();
+    for (String element in b) {
+      if (!setA.contains(element)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   List<String> union(List<String> a, List<String> b) {
-    Set<String> unionSet = {};
+    Set<String> resultSet = {};
     for (String element in a) {
-      unionSet.add(element);
+      resultSet.add(element);
     }
     for (String element in b) {
-      unionSet.add(element);
+      resultSet.add(element);
     }
-    return unionSet.toList();
+    return resultSet.toList();
   }
 
   List<String> interception(List<String> a, List<String> b) {
-    Set<String> interceptionSet = {};
+    Set<String> resultSet = {};
     Set<String> setA = a.toSet();
 
     for (String element in b) {
       if (setA.contains(element)) {
-        interceptionSet.add(element);
+        resultSet.add(element);
       }
     }
-    return interceptionSet.toList();
+    return resultSet.toList();
+  }
+
+  List<String> complement(List<String> a, List<String> b) {
+    List<String> resultList = [];
+    Set<String> setB = b.toSet();
+    for (String element in a) {
+      if (!setB.contains(element)) {
+        resultList.add(element);
+      }
+    }
+    return resultList;
+  }
+
+  List<String> absolute(List<String> a, List<String> b) {
+    List<String> resultList = [];
+    Set<String> setB = b.toSet();
+    for (String element in a) {
+      if (!setB.contains(element)) {
+        resultList.add(element);
+      }
+    }
+    return resultList;
+  }
+
+  List<String> symmetric(List<String> a, List<String> b) {
+    Set<String> resultSet = {};
+    List<String> listA = absolute(a, b);
+    List<String> listB = absolute(b, a);
+    resultSet.addAll(listA);
+    resultSet.addAll(listB);
+    return resultSet.toList();
   }
 }
 
